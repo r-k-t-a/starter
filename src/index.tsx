cf0e787d4f656135892ce2f094707908148a2773
Sergei Droganov <serge@droganov.ru>
2020-04-03 10:11:25 +0300

    add static

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const Application = (): JSX.Element => <div>Application</div>;
-
-ReactDOM.render(<Application />, document.getElementById('app'));
