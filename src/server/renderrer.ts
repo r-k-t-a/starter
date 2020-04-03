479effbbab1f41cd7517c81a8243fda894b436b4
Sergei Droganov <serge@droganov.ru>
2020-04-03 10:11:37 +0300

    trash

---
+++ src/server/renderrer.ts
@@ -0,0 +1,18 @@
+import { Context } from 'koa';
+
+export const renderrer = async (ctx: Context, next: () => Promise<any>) => {
+  ctx.body = `<!DOCTYPE html>
+
+  <html lang="en">
+    <html>
+      <head>
+        <title>koa ejs</title>
+      </head>
+      <body>
+        <div id="app">App</div>
+        <script src="/bundle.js"></script>
+      </body>
+    </html>
+  </html>`;
+  await next();
+};
---
+++ src/server/server.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import Koa from 'koa';
+import Router from 'koa-router';
+import logger from 'koa-logger';
+import bodyParser from 'koa-bodyparser';
+import staticServer from 'koa-static';
+import { renderrer } from './renderrer';
+
+// todo: https://github.com/dougmoscrop/serverless-http
+
+dotenv.config();
+const { PORT } = process.env;
+const app = new Koa();
+const router = new Router();
+
+app
+  .use(logger())
+  .use(bodyParser())
+  .use(staticServer('../public'))
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+router.get('*', renderrer);
+
+app.listen(PORT, () => console.log(`Koa is at http://localhost:${PORT}`));
