05a03e4d4060b0c46b9fc86305b501996f3ba710
Павел Брылев <pavel.brylev@supremedesign.ru>
2020-05-31 22:50:20 +0300

    fix: sw in dev mode (#34)
    * fix: sw in dev mode
    
    * fix(prettier): tune settings

--- webpack.config.js
+++ webpack.config.js
@@ -40,24 +40,24 @@ module.exports = {
   },
   plugins: [
     new webpack.EnvironmentPlugin(publicEnvKeys),
-    isProduction &&
-      new WorkboxPlugin.GenerateSW({
-        additionalManifestEntries: [
-          {
-            revision: Date.now().toString(),
-            url: '/app-shell',
-          },
-        ],
-        cacheId: 'rkta-starter',
-        clientsClaim: true,
-        cleanupOutdatedCaches: true,
-        exclude: [/\.map$/, /^manifest.*\.js(?:on)?$/],
-        skipWaiting: true,
-        swDest: './sw.js',
-        navigateFallback: '/app-shell',
-        navigateFallbackDenylist: [/^\/(b-.*|favicons|sw.js)/],
-      }),
-  ].filter(Boolean),
+    new WorkboxPlugin.GenerateSW({
+      additionalManifestEntries: [
+        {
+          revision: Date.now().toString(),
+          url: '/app-shell',
+        },
+      ],
+      cacheId: 'rkta-starter',
+      clientsClaim: true,
+      cleanupOutdatedCaches: true,
+      exclude: [/\.map$/, /^manifest.*\.js(?:on)?$/],
+      skipWaiting: true,
+      swDest: './sw.js',
+      navigateFallback: '/app-shell',
+      navigateFallbackDenylist: [/^\/(b-.*|favicons|sw.js)/],
+      importScripts: isProduction ? [] : ['sw_unregister.js'],
+    }),
+  ],
   resolve: {
     alias,
     extensions: ['.tsx', '.ts', '.js'],
