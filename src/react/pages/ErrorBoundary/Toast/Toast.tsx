a2175788cd230156bb71cf3adda6ec3a4ac96ca8
Павел Брылев <pavel.brylev@supremedesign.ru>
2020-07-13 18:32:15 +0300

    Deduplicate errors (#45)
    * improve error toast
    
    * chore: deduplicate errors
    
    Co-authored-by: Sergei Droganov <serge@droganov.ru>

--- src/react/pages/ErrorBoundary/Toast/Toast.tsx
+++ src/react/pages/ErrorBoundary/Toast/Toast.tsx
@@ -2,53 +2,48 @@ import { css, jsx } from '@emotion/core';
 import { FC } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState, SHIFT_ERROR } from 'src/react/reducers';
-import { Addon, Button, List, ListItem, ListTitle, useFx } from '@rkta/ui';
+import {
+  Addon,
+  Button,
+  List,
+  ListItem,
+  ListTitle,
+  FloatingArea,
+  useMedia,
+  RktaTheme,
+} from '@rkta/ui';
 import { Cross } from '@rkta/entypo';
+
+const box = (theme: RktaTheme) => css`
+  width: 320px;
+  @media ${theme.media.phone} {
+    width: 100%;
+  }
+`;
 
 export const Toast: FC = (): JSX.Element => {
   const [error] = useSelector((state: RootState) => state.error);
   const dispatch = useDispatch();
-  const [fx, setFx] = useFx('popUp', {
-    onFadeDown: () => {
-      dispatch({ type: SHIFT_ERROR });
-      setFx('popUp');
-    },
-  });
+  const shiftError = () => dispatch({ type: SHIFT_ERROR });
+  const { phone } = useMedia();
+
   return (
-    error && (
-      <List
-        {...fx}
-        bgColor="text"
-        css={[
-          fx.css,
-          css`
-            bottom: 16px;
-            position: fixed;
-            right: 16px;
-            width: 320px;
-            button {
-              margin-right: 8px;
-            }
-          `,
-        ]}
-        color="paper"
-        rize={1}
-      >
+    <FloatingArea
+      active={!!error}
+      align="bottom-right"
+      offset={8}
+      onEscape={shiftError}
+      blockLevel={phone || false}
+    >
+      <List bgColor="text" color="paper" css={box} rize={1}>
         <ListTitle color="error" fitAll>
-          <Addon main>{error.name}</Addon>
-          <Button
-            color="paper"
-            round
-            transparent
-            onClick={(): void => {
-              setFx('fadeDown');
-            }}
-          >
+          <Addon main>{error?.name}</Addon>
+          <Button color="paper" round transparent onClick={shiftError}>
             <Cross />
           </Button>
         </ListTitle>
-        <ListItem>{error.message}</ListItem>
+        <ListItem>{error?.message}</ListItem>
       </List>
-    )
+    </FloatingArea>
   );
 };
